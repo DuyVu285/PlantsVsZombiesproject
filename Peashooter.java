3d7c080399cb431c7d2b1802edb7d5aede5cde99
DuyVu285 <80942615+DuyVu285@users.noreply.github.com>
2022-01-03 21:25:19 +0700

    version before modification

--- Peashooter.java
+++ Peashooter.java
@@ -7,20 +7,8 @@ public class Peashooter extends Plant {
     public Peashooter(GamePanel parent,int x,int y) {
         super(parent,x,y);
             shootTimer = new Timer(2000,(ActionEvent e) -> {
-                if(Checklocation(y))
-                {
-                    shootTimer.stop();
-                    fasterTimer = new Timer(500,(ActionEvent f) -> {
                 if(gp.laneZombies.get(y).size() > 0) {
                     gp.lanePeas.get(y).add(new Pea(gp, y, 103 + this.x * 100));
-                }
-                });
-                fasterTimer.start();
-                }
-                else{
-                if(gp.laneZombies.get(y).size() > 0) {
-                    gp.lanePeas.get(y).add(new Pea(gp, y, 103 + this.x * 100));
-                }
                 }
             });
             shootTimer.start();
